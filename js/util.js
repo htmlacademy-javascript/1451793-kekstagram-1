f1fec529c2cdd7b8c20c7da0cda5746be7e76e44
Denis Morev <morev.denis@gmail.com>
2024-06-12 08:49:48 +0300

    разбить код на модули

---
+++ js/util.js
@@ -0,0 +1,21 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+const createIdGenerator = () => {
+  let lastIdGenerator = 0;
+
+  return () => {
+    lastIdGenerator += 1;
+
+    return lastIdGenerator;
+  };
+};
+
+export { getRandomInteger, getRandomArrayElement, createIdGenerator };
